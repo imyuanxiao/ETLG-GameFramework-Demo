ca18668118296d04fd27692bff62c09232459a79
imyuanxiao <imyuanxiao@126.com>
2023-07-03 11:46:45 +0100

    update dataTables

--- Assets/GameMain/Scripts/Enum/EnumItem.cs
+++ Assets/GameMain/Scripts/Enum/EnumItem.cs
@@ -1,5 +1,5 @@
 ﻿// 此文件由工具自动生成，请勿直接修改。
-// 生成时间：2023-07-02 22:23:59.347
+// 生成时间：2023-07-03 11:42:20.967
 //------------------------------------------------------------
 
 namespace ETLG
@@ -12,28 +12,23 @@ namespace ETLG
         None = 0,
 
         /// <summary>
-        /// 技能信息
-        /// </summary>
-        SkillInfo = 1001,
-
-        /// <summary>
-        /// 配件信息
-        /// </summary>
-        EquipInfo = 1002,
-
-        /// <summary>
         /// NPC选择
         /// </summary>
-        NPCSelect = 1003,
+        NPCSelect = 1001,
 
         /// <summary>
         /// 技能图标
         /// </summary>
-        SkillIcon = 1004,
+        SkillIcon = 1002,
 
         /// <summary>
         /// 道具图标
         /// </summary>
-        ArtifactIcon = 1005,
+        ArtifactIcon = 1003,
+
+        /// <summary>
+        /// 技能升级按钮
+        /// </summary>
+        SkillUpButton = 1004,
     }
 }
